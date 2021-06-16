eb49df5724e62fa8ac3255ef1ac239c1ffd27462
Sunjin Yang <lethean@emstone.com>
2021-06-16 12:23:36 +0900

    build: rename functions

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,9 @@ set -e
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
+# Fetch lethean.github.io submodule
+git submodule update --init --recursive
+
 # Build the project.
 (cd public && rm -rf *)
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
